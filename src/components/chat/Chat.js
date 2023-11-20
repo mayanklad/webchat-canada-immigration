4d3a500fd92fe9cd1b5597a65ed679f94de48618
mayanklad <mayanklad12@gmail.com>
2023-11-19 21:25:26 -0500

    Add initial Chat component

---
+++ src/components/chat/Chat.js
@@ -0,0 +1,11 @@
+import './chat.css';
+
+function Chat() {  
+  return (
+    <div id='chat-container' className='about-container'>
+      This is the Chat component!
+    </div>
+  );
+}
+  
+export default Chat;
